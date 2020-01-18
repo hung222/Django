f1b48c6a58fee03e2b201bc0c6513560e6ab3122
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-19 05:48:09 +0700

    update model

--- model/blog/models.py
+++ model/blog/models.py
@@ -4,3 +4,5 @@ class Post(models.Model):
     title   = models.CharField(max_length=100)
     content = models.TextField()
     date    = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		return self.title
