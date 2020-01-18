3d542f8906c2d2c04271ff6cd4f3ed522bb447e6
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-18 19:07:00 +0700

    *

--- home/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-class SimpleTest(SimpleTestCase)
-    def test_home_page_status(self):
-        response = self.client.get('/home')
-        self.assertEquals(response.status_code, 200)
