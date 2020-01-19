e1406545f6853000796f3e47ebfd1283b82c110c
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-20 00:15:46 +0700

    updated urls.py

--- upload/mywebsite/urls.py
+++ upload/mywebsite/urls.py
@@ -8,8 +8,30 @@ urlpatterns = [
     path('', include('home.urls')),
     path('blog/', include('blog.urls'))
 ]
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import handler400, handler500, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+        # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    ]
+
+
+handler400 = 'home.views.error404'
+handler500 = 'home.views.error500'
 
 handler400 = 'home.views.error404'
 handler500 = 'home.views.error500'
