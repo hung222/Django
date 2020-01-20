ac794501f7e9395ccc65e5ed481f13b92ed84cd8
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-20 09:08:25 +0700

    *

--- login-register-logout/home/form.py
+++ login-register-logout/home/form.py
@@ -4,10 +4,13 @@ from django.core.exceptions import ObjectDoesNotExist
 import re
 
 class RegistrationForm(forms.Form):
+    # Create fields of register
     username  = forms.CharField(label='Tài khoản', max_length=30)
     email     = forms.EmailField(label='Email')
     password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput())
     password2 = forms.CharField(label='Nhập lại mật khẩu', widget=forms.PasswordInput())
+    # Validate for password again of function
+    # Syntax of Django: clean_[field_validate]
     def clean_password2(self):
         if 'password1' in self.cleaned_data:
             password1 = self.cleaned_data['password1']
@@ -24,6 +27,7 @@ class RegistrationForm(forms.Form):
         except ObjectDoesNotExist:
             return username
         raise forms.ValidationError("Tên tài khoản đã tồn tại")
+    # Save info of user to DB
     def save(self):
         User.objects.create_user(username=self.cleaned_data['username'], 
                                             email=self.cleaned_data['email'], 
